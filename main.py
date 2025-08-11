7a2cac57ce90cc65335c5f183693a850f1c15e37
thebeautymasterie-wq <thebeautymaster.ie@gmail.com>
2025-08-11 11:20:00 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Booking bot is running!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
 
